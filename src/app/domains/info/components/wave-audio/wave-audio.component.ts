b2bc91f64ef44d5ae137dffd3e2d40243bad2121
marco-soria <marco.soriaa7@gmail.com>
2024-08-23 18:23:23 -0500

    cycle components

---
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -0,0 +1,61 @@
+import { Component, ElementRef, Input, ViewChild, AfterViewInit, OnDestroy, signal  } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import WaveSurfer from 'wavesurfer.js';
+
+@Component({
+  selector: 'app-wave-audio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wave-audio.component.html',
+  styleUrls: ['./wave-audio.component.css']
+})
+// export class WaveAudioComponent {
+
+//   @Input({required: true}) audioUrl!: string;
+//   @ViewChild('wave') container!: ElementRef;
+//   private ws!: WaveSurfer;
+//   isPlaying = signal(false);
+
+//   ngAfterViewInit() {
+//     this.ws = WaveSurfer.create({
+//       url: this.audioUrl,
+//       container: this.container.nativeElement
+//     });
+//     this.ws.on('play', () => this.isPlaying.set(true));
+//     this.ws.on('pause', () => this.isPlaying.set(false));
+//   }
+
+//   playPause() {
+//     this.ws.playPause();
+//   }
+
+// }
+
+export class WaveAudioComponent implements AfterViewInit, OnDestroy {
+  @Input({required: true}) audioUrl!: string;
+  @ViewChild('wave') container!: ElementRef;
+  private ws!: WaveSurfer;
+  isPlaying = signal(false);
+
+  ngAfterViewInit() {
+    if (typeof document !== 'undefined') {
+      this.ws = WaveSurfer.create({
+        url: this.audioUrl,
+        container: this.container.nativeElement
+      });
+      this.ws.on('play', () => this.isPlaying.set(true));
+      this.ws.on('pause', () => this.isPlaying.set(false));
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.ws) {
+      this.ws.destroy();
+    }
+  }
+
+  playPause() {
+    this.ws.playPause();
+  }
+}
