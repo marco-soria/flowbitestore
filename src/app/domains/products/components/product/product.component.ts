f1ccaec88757f7d8a36122ba8a939945d7899de9
marco-soria <marco.soriaa7@gmail.com>
2024-08-23 20:10:29 -0500

    added header

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Product } from '../../../shared/models/product.model';
 @Component({
   selector: 'app-product',
   standalone: true,
@@ -9,14 +9,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  @Input({required: true}) img: string = '';
-  @Input({required: true}) price: number = 0;
-  @Input({required: true}) title: string = '';
+  @Input({required: true}) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler() {
-    console.log('click form child');
-    this.addToCart.emit('hola este es un msg desde el hijo ' + this.title);
+    //console.log('click form child');
+    this.addToCart.emit('hola este es un msg desde el hijo ' + this.product.title);
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component';
+import { HeaderComponent } from './../../../shared/components/header/header.component';
+import { Product } from './../../../shared/models/product.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[]  = [
+      {
+        id: uuidv4(),
+        title: 'Pro 1',
+        price: 100,
+        image: `https://picsum.photos/640/640?r= + ${Math.random()}`,
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: uuidv4(),
+        title: 'Pro 2',
+        price: 100,
+        image: `https://picsum.photos/640/640?r= + ${Math.random()}`,
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: uuidv4(),
+        title: 'Pro 3',
+        price: 100,
+        image: `https://picsum.photos/640/640?r= + ${Math.random()}`,
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: uuidv4(),
+        title: 'Pro 4',
+        price: 100,
+        image: `https://picsum.photos/640/640?r= + ${Math.random()}`,
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: uuidv4(),
+        title: 'Pro 5',
+        price: 100,
+        image: `https://picsum.photos/640/640?r= + ${Math.random()}`,
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: uuidv4(),
+        title: 'Pro 6',
+        price: 100,
+        image: `https://picsum.photos/640/640?r= + ${Math.random()}`,
+        creationAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string) {
     console.log('estamos en al padre');
     console.log(event);
